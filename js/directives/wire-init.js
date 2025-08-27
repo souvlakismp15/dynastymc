445e2ad80ca45d673cca58bf8652831d33e944e4
souvlaki <dpnminigames@gmail.com>
2025-08-27 21:19:57 +0200

    Add files via upload

---
+++ js/directives/wire-init.js
@@ -0,0 +1,13 @@
+import {
+    directive
+} from "@/directives"
+import Alpine from 'alpinejs'
+
+directive('init', ({
+    el,
+    directive
+}) => {
+    let fullMethod = directive.expression ? ? '$refresh'
+
+    Alpine.evaluate(el, `$wire.${fullMethod}`)
+})
